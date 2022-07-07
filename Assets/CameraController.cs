6493d61eb7493358df5fd9f748280a2665397211
S4co <kacper74@hotmail.com>
2022-07-07 13:56:20 +0200

    CameraController: Follow Player

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.collider.tag == "Obstacle")
+        {
+            //playerMovement.forwardForce = 0f;
+            playerMovement.enabled = false;
+        }
+    }
+}
